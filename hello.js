850079e95c06d9f598276f1a399d7418cb2b219e
MehwishJinnah <80595746+MehwishJinnah@users.noreply.github.com>
2021-03-27 20:45:20 -0400

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,21 @@
+function one() {
+  window.alert("The Great Wall of China is not one long wall, but in fact several shorter structures joined together.")
+}
+function two() {
+  window.alert("Christ the Redeemer was built using reinforced concrete and has an outer shell of 6 million soapstone tiles. It’s believed that the workers who made these tiles occasionally wrote notes on the back, meaning this iconic landmark is full of hidden messages.")
+}
+function three() {
+  window.alert("More than 7,000 feet above sea level in the Andes Mountains, Machu Picchu is the most visited tourist destination in Peru.")
+}
+function four() {
+  window.alert("Chichen Itza is classified as one of the New Seven Wonders of the World and in 1988 was enlisted as an UNESCO World Heritage Site.")
+}
+function five() {
+  window.alert("Built between 72 AD and 80 AD, the Colosseum consists of stone and concrete.")
+}
+function six() {
+  window.alert("It was built as the final resting place for Mumtaz Mahal, the third wife of Mughal emperor Shah Jahan.")
+}
+function seven() {
+  window.alert("Petra is half-built and carved into the rock, and is surrounded by mountains and passages.")
+}
